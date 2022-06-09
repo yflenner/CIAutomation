3a4484ba2e441933f8273798d9ff79ac46d6e277
Yoni Flenner <yoniflenner.net@gmail.com>
2022-06-09 18:00:16 +0300

    Updating to work with Docker

--- src/test/java/demo/Example.java
+++ src/test/java/demo/Example.java
@@ -3,13 +3,21 @@ package demo;
 import com.google.common.util.concurrent.Uninterruptibles;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import sun.plugin2.util.BrowserType;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertEquals;
 
@@ -17,16 +25,36 @@ public class Example {
 
     private WebDriver driver;
 
+//    @BeforeClass
+//    @Parameters({"browserName"})
+//    public void openBrowser(String browser) {
+//        if (browser.equalsIgnoreCase("chrome")) {
+//            WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver();
+//        }
+//        else if (browser.equalsIgnoreCase("firefox")) {
+//            WebDriverManager.firefoxdriver().setup();
+//            driver = new FirefoxDriver();
+//        }
+//        else {
+//            throw new RuntimeException("Wrong Browser Name");
+//        }
+//        driver.manage().window().maximize();
+//        driver.get("https://www.whatsmybrowser.org/");
+//    }
+
+
+    // Working with Docker / Zalenium
     @BeforeClass
     @Parameters({"browserName"})
-    public void openBrowser(String browser) {
+    public void openBrowser(String browser) throws MalformedURLException {
         if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
         }
         else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            DesiredCapabilities cap = DesiredCapabilities.firefox();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
         }
         else {
             throw new RuntimeException("Wrong Browser Name");
